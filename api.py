e554c359d8fd25bd1c3439f9b55bd634727c6745
Kristoffer Alquiza <kalquiza@gmail.com>
2019-12-14 11:15:10 -0800

    Implement CharacterSimple API
    Routes perform CRUD operations and have permissions specified.

--- api.py
+++ api.py
@@ -4,7 +4,49 @@ from character import Character
 
 
 class CharacterSimple(Resource):
-    def get(self, character_id):
+    @requires_auth('get:characters')
+    def get(self, jwt):
+        """GET /characters
+
+        A public endpoint that retrieves the list of characters. Requires the
+        'get:characters' permission.
+
+        Args:
+            jwt: a json web token (string).
+
+        Returns:
+            A status code 200 and json {"success": True, "characters":
+            characters} where characters is the list of characters in the
+            characters.profile() representation or appropriate status code
+            indicating reason for failure.
+        """
+        selection = Character.query.order_by(Character.id).all()
+        characters = [character.profile() for character in selection]
+
+        response = jsonify({
+            'success': True,
+            'character': characters
+        })
+        response.status_code = 200
+        return response
+
+    @requires_auth('get:character')
+    def get(self, character_id, jwt):
+        """GET /characters/<id>
+
+        A public endpoint that retrieves the character for the corresponding
+        row for <id>. Requires the 'get:character' permission.
+
+        Args:
+            jwt: a json web token (string).
+            character_id: where <character_id> is the existing model id (int).
+
+        Returns:
+            A status code 200 and json {"success": True, "character":
+            character} where character is the character in the
+            character.profile() representation or appropriate status code
+            indicating reason for failure.
+        """
         character = Character.query.filter(
             Character.id == character_id).one_or_none()
 
@@ -15,7 +57,23 @@ class CharacterSimple(Resource):
         response.status_code = 200
         return response
 
-    def post(self):
+    @requires_auth('post:character')
+    def post(self, jwt):
+        """POST /characters
+
+        An endpoint that creates a new row in the characters table. Requires
+        the 'post:character' permission.
+
+        Args:
+            jwt: a json web token (string).
+            character_id: where <character_id> is the existing model id (int).
+
+        Returns:
+            A status code 200 and json {"success": True, "character":
+            character} where character is an array containing only the newly
+            created character in the character.profile() representation or
+            appropriate status code indicating reason for failure.
+        """
         try:
             body = request.get_json()
             # Name
@@ -59,6 +117,109 @@ class CharacterSimple(Resource):
             })
             response.status_code = 200
             return response
+
+        except Exception as e:
+            abort(422)
+
+    @requires_auth('patch:character')
+    def patch(self, character_id, jwt):
+        """PATCH /characters/<id>
+
+        An endpoint that updates the corresponding row for <id>. Requires the
+        'patch:character' permission.
+
+        Args:
+            jwt: a json web token (string).
+            character_id: where <character_id> is the existing model id (int).
+
+        Returns:
+            A status code 200 and json {"success": True, "character":
+            character} where character is an array containing only the updated
+            character in the character.profile() representation or appropriate
+            status code indicating reason for failure.
+        """
+        try:
+            character = Character.query.filter(
+                Character.id == character_id).one_or_none()
+
+            if character is None:
+                abort(404)
+
+            body = request.get_json()
+            # Name
+            name = body.get('name', None)
+            # Class Type
+            class_type = body.get('class_type', None)
+            # Age
+            age = body.get('age', None)
+            # Weight in kg
+            weight = body.get('weight', None)
+            # Height in cm
+            height = body.get('height', None)
+            # Blood Type
+            bloodtype = body.get('bloodtype', None)
+            # Three Sizes
+            three_sizes = body.get('three_sizes', None)
+            # Handedness
+            handedness = body.get('handedness', None)
+            # Hobbies
+            hobbies = body.get('hobbies', None)
+            # Astrological Sign
+            astrological_sign = body.get('astrological_sign', None)
+
+            character = Character(
+                name=name,
+                class_type=class_type,
+                age=age,
+                weight=weight,
+                height=height,
+                bloodtype=bloodtype,
+                three_sizes=three_sizes,
+                handedness=handedness,
+                hobbies=hobbies,
+                astrological_sign=astrological_sign
+            )
+            character.update()
+
+            return jsonify({
+                'success': True,
+                'character': [character.profile()]
+            }), 200
+
+        except Exception as e:
+            abort(422)
+
+    @requires_auth('delete:character')
+    def delete(character_id, jwt):
+        """
+            DELETE /characters/<id>:
+                An endpoint that deletes the corresponding row for <id>.
+                Requires the 'delete:character' permission.
+
+            Args:
+                jwt: a json web token (string).
+                character_id: where <character_id> is the existing model id
+                (int).
+
+            Returns:
+                A status code 200 and json {"success": True, "delete": id}
+                where id is the id of the deleted record or appropriate status
+                code indicating reason for failure.
+        """
+        try:
+            character = Character.query.filter(
+                Character.id == character).one_or_none()
+
+            if character is None:
+                abort(404)
+
+            id = character.id
+            character.delete()
+
+            return jsonify({
+                'success': True,
+                'delete': id
+            }), 200
 
         except Exception as e:
             abort(422)
