56442ce63cce94770a1e316741084c4c419710f4
Kristoffer Alquiza <kalquiza@gmail.com>
2019-12-13 21:17:22 -0800

    Update app database configuration

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_cors import CORS
+from database import db_drop_and_create_all, setup_db
+from character import Character
+from card import Card
+from skill import Skill
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
+    setup_db(app)
     CORS(app)
 
     return app
---
+++ database.py
@@ -0,0 +1,40 @@
+import os
+from sqlalchemy import Column, String, Integer
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import json
+from character import Character
+from card import Card
+from skill import Skill
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_path = 'postgres:///unamedidolapp'
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+
+
+def setup_db(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    migrate = Migrate(app, db)
+    db.init_app(app)
+
+
+'''
+db_drop_and_create_all()
+    drops the database tables and starts fresh
+    can be used to initialize a clean database
+    !!NOTE you can change the database_filename variable to have multiple
+    verisons of a database
+'''
+
+
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
